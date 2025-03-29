3fe185f91fd1a4b87f9ecfe6ebd78c602910bf4f
Francelie <francelie.ovalle@gmail.com>
2025-03-28 21:57:59 -0400

    first draft

--- complexnasa/js/main.js
+++ complexnasa/js/main.js
@@ -1,1 +1,70 @@
+document.querySelector('button').addEventListener('click', allOfThem)
 
+function allOfThem (){
+fetch ("https://data.nasa.gov/resource/gvk9-iz74.json")
+.then (res => res.json())
+.then (data => {
+    console.log(data);
+    console.log(data[0]);
+    console.log(data[0].location.latitude)
+    console.log(data[0].location.longitude)
+    data.forEach(theObjects => {
+    console.log(theObjects.center)
+    console.log(theObjects.state)
+    var ul = document.querySelector("#ul")
+    var li = document.createElement("li")
+    li.appendChild(document.createTextNode(theObjects.center))
+    li.appendChild(document.createTextNode(theObjects.state))
+    ul.appendChild(li)
+    fetch  (`http://api.weatherapi.com/v1/forecast.json?key=af481407760e43718b5232620252803&q=${theObjects.location.latitude}, ${theObjects.location.longitude}&days=1&aqi=no&alerts=no`)
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    console.log(data.current);
+    console.log(data.current.temp_f)
+    li.appendChild(document.createTextNode(data.current.temp_f))
+
+    
+})
+.catch(err => {
+    console.log(`err ${err}`);
+    
+})
+    
+    
+})
+    
+})
+.catch(err => {
+    console.log(`error ${err}`);
+    
+})
+}
+
+
+/*Note to self: reference on how to appened an li to ul from to-do list notes 
+
+function addItem() {
+    let userInput = document.querySelector('input').value
+    var ul = document.querySelector("#ul");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(userInput));
+    ul.appendChild(li);
+    document.getElementById("my-form").reset();
+  
+  }*/
+////li.appendChild(document.createTextNode('br'))--Note to self: not able to figure out how to put space between the words , have to follow up, some reading suggest can be fixed with styling, to follow up. 
+/*fetch  ("http://api.weatherapi.com/v1/forecast.json?key=af481407760e43718b5232620252803&q=48.8567, 2.3508&days=1&aqi=no&alerts=no")
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    console.log(data.current);
+    console.log(data.current.temp_f)
+    li.appendChild(document.createTextNode(data.current.temp_f))
+
+    
+})
+.catch(err => {
+    console.log(`err ${err}`);
+    
+})*/
